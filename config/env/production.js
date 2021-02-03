02a30292c0f6c774829ac35df760a90845d9449a
Kevin Guzman <kevin.guzman@udla.edu.ec>
2021-02-02 21:48:56 -0500

    Errores

--- config/env/production.js
+++ config/env/production.js
@@ -70,7 +70,7 @@ module.exports = {
       * https://sailsjs.com/config/datastores                                     *
       *                                                                           *
       ****************************************************************************/
-      ssl: true,
+      ssl: false,
 
     },
 
@@ -340,7 +340,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  // port: 80,
+  port: process.env.PORT,
 
 
 
